1a9a95536657232513a8e768d2563e4565f1d960
tybuu <tybuu1234@gmail.com>
2025-06-05 08:34:37 -0700

    got working implementtation with 600 and 400 khz

--- src/bin/no_antenna.rs
+++ src/bin/no_antenna.rs
@@ -10,15 +10,15 @@ use embassy_executor::{Executor, Spawner};
 use embassy_futures::join::{join, join4};
 use embassy_futures::select::select;
 use embassy_futures::yield_now;
+use embassy_rp::clocks::{clk_sys_freq, ClockConfig};
+use embassy_rp::config::Config;
 use embassy_rp::gpio::{Input, Level, Output, Pin, Pull};
 use embassy_rp::multicore::{spawn_core1, Stack};
 use embassy_rp::pac::PWM;
 use embassy_rp::peripherals::{DMA_CH0, PIN_15, PIN_16, PIN_17, PIO0, PWM_SLICE7};
-use embassy_rp::pio::program::pio_asm;
-use embassy_rp::pio::{Config, Direction, Pio};
 use embassy_rp::pwm::{self, Slice};
 use embassy_rp::pwm::{Pwm, SetDutyCycle};
-use embassy_rp::{dma, pac, PeripheralRef};
+use embassy_rp::{clocks, dma, pac, Peri};
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 use embassy_sync::channel::Channel;
 use embassy_time::{Duration, Instant, Ticker, Timer};
@@ -42,18 +42,19 @@ const BUFFER_SIZE: usize = BARKER_CODE.len() + NUM_DATA_BITS * REPITITION_SIZE;
 async fn main(_spawner: Spawner) {
     info!("Device Started!");
     info!("Buffer Size: {}", BUFFER_SIZE);
-
+    // let clk_cfg = ClockConfig::system_freq(200_000_000).unwrap();
+    // let p = embassy_rp::init(Config::new(clk_cfg));
     let p = embassy_rp::init(Default::default());
     let _ = Output::new(p.PIN_25, Level::High);
 
-    let dma0 = PeripheralRef::new(p.DMA_CH0);
+    let dma0 = p.DMA_CH0;
     let mut c_high = pwm::Config::default();
-    c_high.top = 624;
-    c_high.compare_b = 312;
+    c_high.top = 207;
+    c_high.compare_b = 108;
 
     let mut c_low = pwm::Config::default();
-    c_low.top = 1249;
-    c_low.compare_b = 625;
+    c_low.top = 311;
+    c_low.compare_b = 156;
 
     let high_pwm = Pwm::new_output_b(p.PWM_SLICE7, p.PIN_15, c_high);
     let low_pwm = Pwm::new_output_b(p.PWM_SLICE6, p.PIN_13, c_low);
@@ -82,6 +83,10 @@ async fn main(_spawner: Spawner) {
                 bits.set_bits(current_state);
                 info!("{}", bits.get_ref());
                 tranmitter.transmit_data(bits.get_ref()).await;
+                // tranmitter
+                //     .transmit_data_timed(bits.get_ref(), Duration::from_millis(500))
+                //     .await;
+                Timer::after_micros(500).await;
             }
             if !debouncers[0].is_pressed() && !debouncers[1].is_pressed() {
                 break;
@@ -91,20 +96,20 @@ async fn main(_spawner: Spawner) {
 }
 
 struct Transmitter {
-    dma: PeripheralRef<'static, DMA_CH0>,
+    dma: Peri<'static, DMA_CH0>,
     reg: *mut u32,
 }
 
 impl Transmitter {
-    fn new(dma: PeripheralRef<'static, DMA_CH0>, reg: *mut u32) -> Self {
+    fn new(dma: Peri<'static, DMA_CH0>, reg: *mut u32) -> Self {
         // Disable all pwm slices
         unsafe {
             reg.write_volatile(0);
         }
-        // Set the DMA timer to trigger every 50 us
+        // Set the DMA timer to trigger every 100 us
         pac::DMA.timer(0).modify(|w| {
             w.set_x(1);
-            w.set_y(12600);
+            w.set_y(12500);
         });
         Self { dma, reg }
     }
@@ -120,7 +125,20 @@ impl Transmitter {
             )
             .await;
         }
+
         // Disable all pwm slices
+        unsafe {
+            self.reg.write_volatile(0);
+        }
+    }
+
+    async fn transmit_data_timed(&mut self, data: &[u32], dur: Duration) {
+        for bit in data {
+            unsafe {
+                self.reg.write_volatile(*bit);
+            }
+            Timer::after(dur).await;
+        }
         unsafe {
             self.reg.write_volatile(0);
         }
