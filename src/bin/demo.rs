28db7bd9cc504d7fe1c6d0ff37074ea914ba5183
tybuu <tybuu1234@gmail.com>
2025-06-15 17:07:11 -0700

    added error test files to find error rates and added documentation

--- src/bin/demo.rs
+++ src/bin/demo.rs
@@ -33,70 +33,42 @@ static CHANNEL: Channel<CriticalSectionRawMutex, [bool; 2], 5> = Channel::new();
 const HIGH: u32 = 1 << 4;
 const LOW: u32 = 2 << 4;
 
-const BARKER_CODE: [u32; 7] = [HIGH, HIGH, HIGH, LOW, LOW, HIGH, LOW];
-const REPITITION_SIZE: usize = 3;
-const NUM_DATA_BITS: usize = 2;
-const BUFFER_SIZE: usize = BARKER_CODE.len() + NUM_DATA_BITS * REPITITION_SIZE;
-
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     info!("Device Started!");
     // let clk_cfg = ClockConfig::system_freq(200_000_000).unwrap();
     // let p = embassy_rp::init(Config::new(clk_cfg));
-
     let p = embassy_rp::init(Default::default());
     let _ = Output::new(p.PIN_25, Level::High);
 
     let dma0 = p.DMA_CH0;
-
-    // Configured for 600 KHz
     let mut c_high = pwm::Config::default();
-    c_high.top = 207;
-    c_high.compare_b = 104;
+    // c_high.top = 207;
+    // c_high.compare_b = 104;
+    c_high.top = 1249;
+    c_high.compare_b = 625;
 
-    // Set the drive strength to its highest value to have stronger signals
+    // Make the transitions between low and high sharper to have stronger signals
     PADS_BANK0.gpio(15).modify(|x| {
         x.set_drive(pac::pads::vals::Drive::_12M_A);
     });
 
-    // Unused as we use register writes to configure the PWM but needs to be kept as Embassy disables the peripheral if dropped
     let high_pwm = Pwm::new_output_b(p.PWM_SLICE7, p.PIN_15, c_high);
-
-    // Disable ch7
-    pac::PWM.en().write(|x| x.set_ch7(false));
-
-    let reg = PWM.ch(7).div().as_ptr() as *mut u32;
-    let mut tranmitter = Transmitter::new(dma0, reg);
-
-    let mut inputs = [
-        Input::new(p.PIN_17, Pull::Down),
-        Input::new(p.PIN_16, Pull::Down),
-    ];
-
-    let mut debouncers = [Debouncer::default(); 2];
-    let mut states = [false, false];
-    let mut bits = Bits::default();
+    // let reg = PWM.ch(7).div().as_ptr() as *mut u32;
+    // PWM.en().modify(|x| x.set_ch7(false));
     loop {
-        let (i0, i1) = inputs.split_at_mut(1);
-        // select(i0[0].wait_for_high(), i1[0].wait_for_high()).await;
         loop {
-            let mut current_state = states;
-            debouncers[0].update_buf(inputs[0].get_level().into());
-            debouncers[1].update_buf(inputs[1].get_level().into());
-            current_state[0] = debouncers[0].is_pressed();
-            current_state[1] = debouncers[1].is_pressed();
-            if current_state != states {
-                states = current_state;
-                bits.set_bits(current_state);
-                info!("{}", bits.get_ref());
-                tranmitter.transmit_data(bits.get_ref()).await;
-                // tranmitter
-                //     .transmit_data_timed(bits.get_ref(), Duration::from_millis(500))
-                //     .await;
-            }
-            if !debouncers[0].is_pressed() && !debouncers[1].is_pressed() {
-                break;
-            }
+            // unsafe {
+            //     reg.write(HIGH);
+            // }
+            PWM.ch(7).div().write(|x| x.set_int(1));
+            Timer::after_millis(500).await;
+
+            PWM.ch(7).div().write(|x| x.set_int(2));
+            // unsafe {
+            //     reg.write(LOW);
+            // }
+            Timer::after_millis(500).await;
         }
     }
 }
@@ -115,15 +87,13 @@ impl Transmitter {
         // Set the DMA timer to trigger every 100 us
         pac::DMA.timer(0).modify(|w| {
             w.set_x(1);
-            w.set_y(3125);
+            w.set_y(12500);
         });
         Self { dma, reg }
     }
-
     async fn transmit_data(&mut self, data: &[u32]) {
         // Transfer data to the PWM enable register to change which slices are active
         // to have certain frequencies at a bit period
-        pac::PWM.en().write(|x| x.set_ch7(true));
         unsafe {
             dma::write(
                 self.dma.reborrow(),
@@ -133,47 +103,22 @@ impl Transmitter {
             )
             .await;
         }
-        // We need delay as after last value is sent, it automatically goes to the next line
-        Timer::after_micros(25).await;
 
         // Disable all pwm slices
-        pac::PWM.en().write(|x| x.set_ch7(false));
-        Timer::after_micros(250).await;
+        unsafe {
+            self.reg.write_volatile(0);
+        }
     }
 
     async fn transmit_data_timed(&mut self, data: &[u32], dur: Duration) {
-        pac::PWM.en().write(|x| x.set_ch7(true));
         for bit in data {
             unsafe {
                 self.reg.write_volatile(*bit);
             }
             Timer::after(dur).await;
         }
-        pac::PWM.en().write(|x| x.set_ch7(false));
-    }
-}
-
-struct Bits {
-    bits: [u32; BUFFER_SIZE],
-}
-
-impl Bits {
-    fn default() -> Self {
-        let mut bits = [LOW; BUFFER_SIZE];
-        bits[0..BARKER_CODE.len()].copy_from_slice(&BARKER_CODE);
-        Self { bits }
-    }
-
-    fn set_bits(&mut self, data: [bool; NUM_DATA_BITS]) {
-        for i in 0..NUM_DATA_BITS {
-            let start = BARKER_CODE.len() + i * REPITITION_SIZE;
-            let end = start + REPITITION_SIZE;
-            let val = if data[i] { HIGH } else { LOW };
-            self.bits[start..end].iter_mut().for_each(|x| *x = val);
+        unsafe {
+            self.reg.write_volatile(0);
         }
-    }
-
-    fn get_ref(&self) -> &[u32] {
-        &self.bits
     }
 }
--- src/bin/error_test.rs
+++ src/bin/error_test.rs
@@ -33,9 +33,15 @@ static CHANNEL: Channel<CriticalSectionRawMutex, [bool; 2], 5> = Channel::new();
 const HIGH: u32 = 1 << 4;
 const LOW: u32 = 2 << 4;
 
+const BARKER_CODE: [u32; 7] = [HIGH, HIGH, HIGH, LOW, LOW, HIGH, LOW];
+const REPITITION_SIZE: usize = 3;
+const NUM_DATA_BITS: usize = 2;
+const BUFFER_SIZE: usize = BARKER_CODE.len() + NUM_DATA_BITS * REPITITION_SIZE;
+
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     info!("Device Started!");
+    info!("Buffer Size: {}", BUFFER_SIZE);
     // let clk_cfg = ClockConfig::system_freq(200_000_000).unwrap();
     // let p = embassy_rp::init(Config::new(clk_cfg));
     let p = embassy_rp::init(Default::default());
@@ -47,23 +53,25 @@ async fn main(_spawner: Spawner) {
     c_high.compare_b = 104;
 
     // Make the transitions between low and high sharper to have stronger signals
-    // PADS_BANK0.gpio(15).modify(|x| {
-    //     x.set_drive(pac::pads::vals::Drive::_12M_A);
-    // });
+    PADS_BANK0.gpio(15).modify(|x| {
+        x.set_drive(pac::pads::vals::Drive::_12M_A);
+    });
 
     let high_pwm = Pwm::new_output_b(p.PWM_SLICE7, p.PIN_15, c_high);
+    // Disable ch7
+    pac::PWM.en().write(|x| x.set_ch7(false));
+
     let reg = PWM.ch(7).div().as_ptr() as *mut u32;
+    let mut tranmitter = Transmitter::new(dma0, reg);
+
+    let mut states = [false, true];
+    let mut bits = Bits::default();
     loop {
-        loop {
-            unsafe {
-                reg.write(HIGH);
-            }
-            Timer::after_millis(500).await;
-            unsafe {
-                reg.write(LOW);
-            }
-            Timer::after_millis(500).await;
-        }
+        states[0] = !states[0];
+        states[1] = !states[1];
+        bits.set_bits(states);
+        tranmitter.transmit_data(bits.get_ref()).await;
+        Timer::after_micros(1500).await;
     }
 }
 
@@ -81,13 +89,15 @@ impl Transmitter {
         // Set the DMA timer to trigger every 100 us
         pac::DMA.timer(0).modify(|w| {
             w.set_x(1);
-            w.set_y(12500);
+            w.set_y(3125);
         });
         Self { dma, reg }
     }
+
     async fn transmit_data(&mut self, data: &[u32]) {
         // Transfer data to the PWM enable register to change which slices are active
         // to have certain frequencies at a bit period
+        pac::PWM.en().write(|x| x.set_ch7(true));
         unsafe {
             dma::write(
                 self.dma.reborrow(),
@@ -97,22 +107,45 @@ impl Transmitter {
             )
             .await;
         }
-
+        Timer::after_micros(25).await;
         // Disable all pwm slices
-        unsafe {
-            self.reg.write_volatile(0);
-        }
+        pac::PWM.en().write(|x| x.set_ch7(false));
+        // Timer::after_micros(250).await;
     }
 
     async fn transmit_data_timed(&mut self, data: &[u32], dur: Duration) {
+        pac::PWM.en().write(|x| x.set_ch7(true));
         for bit in data {
             unsafe {
                 self.reg.write_volatile(*bit);
             }
             Timer::after(dur).await;
         }
-        unsafe {
-            self.reg.write_volatile(0);
+        pac::PWM.en().write(|x| x.set_ch7(false));
+    }
+}
+
+struct Bits {
+    bits: [u32; BUFFER_SIZE],
+}
+
+impl Bits {
+    fn default() -> Self {
+        let mut bits = [LOW; BUFFER_SIZE];
+        bits[0..BARKER_CODE.len()].copy_from_slice(&BARKER_CODE);
+        Self { bits }
+    }
+
+    fn set_bits(&mut self, data: [bool; NUM_DATA_BITS]) {
+        for i in 0..NUM_DATA_BITS {
+            let start = BARKER_CODE.len() + i * REPITITION_SIZE;
+            let end = start + REPITITION_SIZE;
+            let val = if data[i] { HIGH } else { LOW };
+            self.bits[start..end].iter_mut().for_each(|x| *x = val);
         }
+    }
+
+    fn get_ref(&self) -> &[u32] {
+        &self.bits
     }
 }
